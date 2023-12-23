a3c495959c531c3cdfc210b62d1facab915f073b
Fatih Alp <fatihalp89@gmail.com>
2023-12-24 01:29:16 +0300

    Twig 3 Update

--- src/FilesModulePlugin.php
+++ src/FilesModulePlugin.php
@@ -5,6 +5,7 @@ use Anomaly\FilesModule\File\Command\GetMaxUploadSize;
 use Anomaly\FilesModule\Folder\Command\GetFolder;
 use Anomaly\Streams\Platform\Addon\Plugin\Plugin;
 use Anomaly\Streams\Platform\Support\Decorator;
+use Twig\TwigFunction;
 
 /**
  * Class FilesModulePlugin
@@ -24,19 +25,19 @@ class FilesModulePlugin extends Plugin
     public function getFunctions()
     {
         return [
-            new \Twig_SimpleFunction(
+            new TwigFunction(
                 'max_upload_size',
                 function () {
                     return dispatch_sync(new GetMaxUploadSize());
                 }
             ),
-            new \Twig_SimpleFunction(
+            new TwigFunction(
                 'file',
                 function ($identifier) {
                     return (new Decorator())->decorate(dispatch_sync(new GetFile($identifier)));
                 }
             ),
-            new \Twig_SimpleFunction(
+            new TwigFunction(
                 'folder',
                 function ($identifier) {
                     return (new Decorator())->decorate(dispatch_sync(new GetFolder($identifier)));
